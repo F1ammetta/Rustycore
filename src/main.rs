2d1ca8e797d5c89390a854e300b758a4cb896251
Sergio <sdepineresramirez@utb.edu.co>
2024-04-06 16:26:39 -0500

    :o

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+#![windows_subsystem = "windows"]
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
-use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
+// use openssl::ssl::{SslAcceptor, SslFiletype, SslMethod};
 mod database;
 use dotenv::dotenv;
 
@@ -49,7 +50,8 @@ async fn track(path: web::Path<String>) -> impl Responder {
 #[get("/app")]
 async fn app() -> impl Responder {
     println!("GET: App");
-    let path = std::env::var("APP_PATH").unwrap();
+    let path =
+        "C:\\Users\\Sergio\\flutter\\soncore\\build\\app\\outputs\\flutter-apk\\app-release.apk";
     HttpResponse::Ok()
         .content_type("application/vnd.android.package-archive")
         .append_header(("Access-Control-Allow-Origin", "*"))
@@ -65,16 +67,19 @@ async fn main() -> std::io::Result<()> {
             database::update_db().unwrap();
         }
     }
-    let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
+    // let mut builder = SslAcceptor::mozilla_intermediate(SslMethod::tls()).unwrap();
     dotenv().ok();
     // set these env variables if you wanna use SSL
-    let cert = std::env::var("CERT_CHAIN").unwrap();
-    let key = std::env::var("KEY").unwrap();
+    // let cert = "C:\\Certbot\\live\\music.soncore.net\\fullchain.pem";
+    // let key = "C:\\Certbot\\live\\music.soncore.net\\privkey.pem";
+    // println!("CERT_CHAIN: {}", cert);
+    // println!("KEY: {}", key);
+    // builder.set_private_key_file(key, SslFiletype::PEM).unwrap();
+    // builder.set_certificate_chain_file(cert).unwrap();
     // env variable for the ip address (will implement a config file later)
-    let addr = std::env::var("ADDR").unwrap();
+    let addr = "127.0.0.1";
 
-    builder.set_private_key_file(key, SslFiletype::PEM).unwrap();
-    builder.set_certificate_chain_file(cert).unwrap();
+    println!("Starting server at: {}", addr);
 
     HttpServer::new(|| {
         App::new()
@@ -83,8 +88,9 @@ async fn main() -> std::io::Result<()> {
             .service(cover)
             .service(app)
     })
+    .bind(format!("{}:42069", addr))?
     // change this to just bind if you're not using SSL (also mind the port)
-    .bind_openssl(format!("{}:443", addr), builder)?
+    // .bind_openssl(format!("{}:42069", addr), builder)?
     .run()
     .await
 }
